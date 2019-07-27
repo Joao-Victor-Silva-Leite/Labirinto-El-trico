b616309dc3f44fc81c71b09ceedd566b8458f08b
Joao-Victor-Silva-Leite <53185153+Joao-Victor-Silva-Leite@users.noreply.github.com>
2019-07-27 12:25:56 -0300

    Update labirinto_eletrico.ino

--- Labirinto-El-trico/labirinto_eletrico.ino
+++ Labirinto-El-trico/labirinto_eletrico.ino
@@ -1,25 +1,26 @@
-/* *********************** Nervo Teste do Manual do Mundo ***********************
-   Criado por: Anderson Harayashiki Moreira
-   Rev.: 02
-   Data: 25.02.2018
+/* **********************************************
    Guia de conexão:
    LED indicativo: Pino 2
    Labirinto: Pino 3
+   Concluir labirinto: Pino 4
    Botão: Pino 7
    Buzzer: Pino 9
    Módulo Relé (Sirene): Pino 10
- ***************************************************************************** */
+ ************************************************ */
 // Declaração dos pinos utilizados do Arduino
 int pinoLED = 2;
 int pinoLabirinto = 3;
+int pinovitoria = 4;
 int pinoBuzzer = 9;
 int pinoSirene = 10;
+int vitoria = 4;
 // Declaração das variáveis auxiliares
 int nivel = 0;
 long int tempoAnterior = 0;
 int deltaT = 200;
 int tomBuzzer = 3000;
 int estadoBotao = LOW;
+int winif = LOW;
 int estadoBuzzer = LOW;
 
 // A função setup é apenas executada uma vez ao se iniciar o Arduino, aqui configuramos nossa placa Arduino
@@ -28,6 +29,7 @@ void setup() {
   // Pinos configurados como entrada:
   pinMode(pinoLabirinto, INPUT_PULLUP); // Configurando a entrada como INPUT_PULLUP, utilizamos o resistor interno pull-up do Arduino
   //=======  pinMode(pinoBotao, INPUT);
+  pinMode(pinovitoria, INPUT_PULLUP);
   // Pinos configurados como saída:
   pinMode(pinoBuzzer, OUTPUT);
   pinMode(pinoSirene, OUTPUT);
@@ -41,6 +43,21 @@ void setup() {
 void loop() {
   Serial.println(nivel);
   estadoBotao = digitalRead(pinoLabirinto);
+  winif = digitalRead(vitoria);
+  if (winif == LOW) {
+    digitalWrite(pinoLED, LOW); //Quando o pino 4 se encontrar com o neutro, o led que ligava quando o circuito ligava, agora vai piscar algumas vezes anunciando a vitoria.
+    delay (2000);
+    digitalWrite(pinoLED, HIGH);
+    delay (1000);
+    digitalWrite(pinoLED, LOW);
+    delay (500);
+    digitalWrite(pinoLED, HIGH);
+    delay (1500);
+    digitalWrite(pinoLED, LOW);
+    delay (500);
+    digitalWrite(pinoLED, HIGH);
+    delay (1500);
+  }
   if (estadoBotao == LOW) {
     delay(50);
     estadoBotao = digitalRead(pinoLabirinto);
@@ -83,5 +100,4 @@ void loop() {
     tempoAnterior = millis();
   }
 }
-
 
